390e21803e8b004843d5e9c0730e3192e5b7ba58
Osmo Salomaa <otsaloma@iki.fi>
2016-07-23 20:52:19 +0300

    Update for 1.20

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 // -*- coding: utf-8-unix -*-
 
 $(document).on("ready", function() {
-    // TODO: When we have a Windows installer.
-    // if (platform.os.family.indexOf("Windows") === 0) {
-    //     $(".download.source").hide();
-    //     $(".download.windows").show();
-    // }
+    if (platform.os.family.indexOf("Windows") === 0) {
+        $(".download.source").hide();
+        $(".download.windows").show();
+    }
 });
